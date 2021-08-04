9c4e7b1fe16f638eb92bfa52353449d7472e1195
midzovicmilan <76958020+midzovicmilan@users.noreply.github.com>
2021-08-05 01:15:58 +0200

    Add files via upload

---
+++ js/ground.js
@@ -0,0 +1,10 @@
+import { InfiniteScrollingBackground } from "./infiniteScrollingBackground.js";
+
+export class Ground extends InfiniteScrollingBackground {
+  constructor(ctx) {
+    super(ctx);
+
+    this.velocity = 2;
+    this.spritesheetUrl = "spritesheets/ground.png";
+  }
+}
---
+++ js/stars.js
@@ -0,0 +1,10 @@
+import { InfiniteScrollingBackground } from "./infiniteScrollingBackground.js";
+
+export class Stars extends InfiniteScrollingBackground {
+  constructor(ctx) {
+    super(ctx);
+
+    this.velocity = 1;
+    this.spritesheetUrl = "spritesheets/stars.png";
+  }
+}
